31b42f349638b7eaca0b2215d4b6baca41e89310
Mazen <mazenadel19@gmail.com>
2024-09-23 22:39:50 +0300

    chore: remove unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-// styles
-import './App.css'
 
 function App() {
     return <AppRoutes />
